2de358f76b176fcdb810b801c41086160b3101cc
CyberRoshan <roshanchaurasia990@gmail.com>
2024-10-04 16:51:31 +0530

    testing

--- src/app/modals/MobileSideBar.jsx
+++ src/app/modals/MobileSideBar.jsx
@@ -26,7 +26,7 @@ export default function MobileSideBar({sidebarStatus}) {
       </DisclosureButton>
       <DisclosurePanel className=" duration-500 ">
       <ul className="space-y-2">
-            <li className="text-[15px] font-semibold cursor-pointer hover:underline">
+            <li className="text-[15px] font-semibold cursor-pointer hover:underline bg-red-700">
               Shop All
             </li>
             <li className="text-[15px] font-semibold cursor-pointer hover:underline">
